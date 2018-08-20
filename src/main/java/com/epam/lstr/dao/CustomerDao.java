496379d830ed62da6404c1f716ec6ae790181fd4
Pavel Agapov <pavel_agapov@epam.com>
2018-08-20 20:12:03 +0300

    62 - Create Customers DAO

---
+++ src/main/java/com/epam/lstr/dao/CustomerDao.java
@@ -0,0 +1,25 @@
+package com.epam.lstr.dao;
+
+import com.epam.lstr.dao.model.Customer;
+
+import java.util.List;
+
+/**
+ * Interface of CustomerDao
+ *
+ * @author Pavel Agapov
+ * @version 1.0
+ */
+
+public interface CustomerDao {
+
+    Customer create(Customer customer);
+    Customer update(Customer customer);
+    CustomerDao delete(Customer customer);
+
+    Customer getCustomerById(int id);
+    List<Customer> getAllCustomers();
+
+    CustomerDao clear();
+    int count();
+}
---
+++ src/test/java/com/epam/lstr/dao/impl/CustomerDaoImplTest.java
@@ -0,0 +1,113 @@
+package com.epam.lstr.dao.impl;
+
+import com.epam.lstr.dao.CustomerDao;
+import com.epam.lstr.dao.model.Customer;
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class CustomerDaoImplTest {
+
+    final static String CUSTOMER_NAME = "Don";
+    final static String CUSTOMER_SURNAME = "Pomodoro";
+    final static boolean CUSTOMER_VIP = true;
+    final static int CUSTOMER_USER_ID = 666;
+
+    final static int ROW_COUNT = 3;
+
+    static Customer customer = newCustomer();
+    static CustomerDao customerDao = new CustomerDaoImpl();
+
+    private static Customer newCustomer() {
+        return new Customer(
+                CUSTOMER_NAME,
+                CUSTOMER_SURNAME,
+                CUSTOMER_VIP,
+                CUSTOMER_USER_ID
+        );
+    }
+
+    @Test
+    @DisplayName("Create method works correctly.")
+    void create() {
+        customerDao.create(customer);
+        assertThat(customerDao.getCustomerById(customer.getCustomerId()), is(customer));
+
+        customerDao.delete(customer);
+    }
+
+    @Test
+    @DisplayName("Update method works correctly.")
+    void update() {
+        customerDao.create(customer);
+
+        customer.setName("Donna");
+        customer.setSurname("Pomodorio");
+        customer.setVIP(false);
+        customerDao.update(customer);
+
+        assertThat(customerDao.getCustomerById(customer.getCustomerId()), is(customer));
+
+        customerDao.delete(customer);
+    }
+
+    @Test
+    @DisplayName("Delete method works correctly.")
+    void delete() {
+        customerDao.create(customer);
+        customerDao.delete(customer);
+
+        assertThat(customerDao.count(), is(0));
+    }
+
+    @Test
+    @DisplayName("GetCustomerById method works correctly.")
+    void getCustomerById() {
+        customerDao.create(customer);
+
+        assertThat(customerDao.getCustomerById(customer.getCustomerId()), is(customer));
+
+        customerDao.delete(customer);
+    }
+
+    @Test
+    @DisplayName("GetAllCustomers method works correctly.")
+    void getAllCustomers() {
+        for (int i = 0; i < ROW_COUNT; i++) {
+            customerDao.create(customer);
+        }
+
+        assertThat(customerDao.getAllCustomers().size(), is(ROW_COUNT));
+
+        customerDao.clear();
+    }
+
+    @Test
+    @DisplayName("Clear method works correctly.")
+    void clear() {
+        for (int i = 0; i < ROW_COUNT; i++) {
+            customerDao.create(customer);
+        }
+
+        customerDao.clear();
+
+        assertThat(customerDao.count(), is(0));
+    }
+
+    @Test
+    @DisplayName("Count method works correctly.")
+    void count() {
+        for (int i = 0; i < ROW_COUNT; i++) {
+            customerDao.create(customer);
+        }
+
+        assertThat(customerDao.count(), is(ROW_COUNT));
+
+        customerDao.clear();
+    }
+}
